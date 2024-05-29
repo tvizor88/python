2c07f99a28ca9d6cec1278099ce26824d685107d
NBK-278\Aleksandr Gorshkov <aleksandr.gorshkov@chisw.com>
2024-05-29 18:16:38 +0300

    small changes

--- lection_2/gorshkov2.py
+++ lection_2/gorshkov2.py
@@ -10,13 +10,14 @@ import re  # import module to work with patterns
 
 
 def count_hi(times):  # declarate the function
-    return times.count('hi')  # declarate what should be counted
+    return times.count("hi")  # declarate what should be counted
 
 
-print(count_hi('abc hi ho'))  # work with string
-print(count_hi('ABChi hi'))  # work with string
-print(count_hi('hihi'))  # work with string
-print('______________________')  # separate tasks results
+print(count_hi("abc hi ho"))  # work with string
+print(count_hi("ABChi hi"))  # work with string
+print(count_hi("hihi"))  # work with string
+print("______________________")  # separate tasks results
+
 """
 Print the number of even ints in the given list. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
 [2, 1, 2, 3, 4] -> 3
@@ -32,7 +33,7 @@ def count_even_numbers(nums):  # declarate the function
 print(count_even_numbers([2, 1, 2, 3, 4]))  # print the result
 print(count_even_numbers([2, 2, 0]))  # print the result
 print(count_even_numbers([1, 3, 5]))  # print the result
-print('______________________')  # separate tasks results
+print("______________________")  # separate tasks results
 
 """
 Remove all the occurrences of an element from a list and print resulting list
@@ -43,16 +44,18 @@ Remove all the occurrences of an element from a list and print resulting list
 
 
 def remove_element(element, array):  # declarate the function
-    return [number for number in array if number != element]  # describing the formula excepting search element
+    return [
+        number for number in array if number != element
+    ]  # describing the formula excepting search element
 
 
 print(remove_element(1, [1, 1, 2, 3, 4, 5, 1, 2]))  # print reformated array
 print(remove_element(7, [5, 6, 7, 8, 9, 10, 7]))  # print reformated array
 print(remove_element(4, [1, 2, 3]))  # print reformated array
-print('______________________')  # separate tasks results
+print("______________________")  # separate tasks results
 
 """
-Given list of Strings, your task is to add a space before sequence which 
+Given list of Strings, your task is to add a space before sequence which
 begins with capital letters to make separate words.
 ['everyOne', 'lovesPython', 'becauseItIsEasy'] -> ['every One', 'loves Python', 'because It Is Easy']
 ['LoremIpsumDolorSitAmet'] -> [‘Lorem Ipsum Dolor Sit Amet’]
@@ -61,18 +64,26 @@ begins with capital letters to make separate words.
 
 
 def add_space(s):  # declarate the function
-    return re.sub(r'(?=[A-Z])', ' ', s)  # declarate pattern that search Upper letter
+    return re.sub(r"(?=[A-Z])", " ", s)  # declarate pattern that search Upper letter
 
 
-strings = ['everyOne', 'lovesPython', 'becauseItIsEasy',
-           'LoremIpsumDolorSitAmet', 'onlysmall', 'lettershere']  # input string
-formatted_strings = [add_space(s) for s in strings]  # creating reformated string + spaces
+strings = [
+    "everyOne",
+    "lovesPython",
+    "becauseItIsEasy",
+    "LoremIpsumDolorSitAmet",
+    "onlysmall",
+    "lettershere",
+]  # input string
+formatted_strings = [
+    add_space(s) for s in strings
+]  # creating reformated string + spaces
 print(formatted_strings)  # printing output string
-print('______________________')  # separate tasks results
+print("______________________")  # separate tasks results
 
 """
-Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, 
-or whatever is there if the string is less than length 3. 
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars,
+or whatever is there if the string is less than length 3.
 Print n copies of the front
 'Chocolate', 2 -> 'ChoCho'
 'Chocolate', 3 -> 'ChoChoCho'
@@ -85,13 +96,13 @@ def repeater(cut, n):  # function declaration
     return word * n  # duplicate cut letters
 
 
-print(repeater('Chocolate', 2))  # print result
-print(repeater('Chocolate', 3))  # print result
-print(repeater('Ab', 3))  # print result
-print('______________________')  # separate tasks results
+print(repeater("Chocolate", 2))  # print result
+print(repeater("Chocolate", 3))  # print result
+print(repeater("Ab", 3))  # print result
+print("______________________")  # separate tasks results
 
 """
-Given a list of ints, return True if one of the last 4 elements in the list is a 9. 
+Given a list of ints, return True if one of the last 4 elements in the list is a 9.
 The list length may be less than 4.
 [1, 2, 9, 3, 4] -> True
 [9, 1, 2, 3, 4] -> False
@@ -106,11 +117,11 @@ def check_nine(nums):  # declarate function
 print(check_nine([1, 2, 9, 3, 4]))  # print result
 print(check_nine([9, 1, 2, 3, 4]))  # print result
 print(check_nine([1, 2, 3]))  # print result
-print('______________________')  # separate tasks results
+print("______________________")  # separate tasks results
 
 """
-The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. 
-In this example, the "i" tag makes <i> and </i> which surround the word "Yay". 
+The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text.
+In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
 Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
 'i', 'Yay' -> '<i>Yay</i>'
 'i', 'Hello' -> '<i>Hello</i>'
@@ -119,18 +130,18 @@ Given tag and word strings, create the HTML string with tags around the word, e.
 
 
 def html(tag, word):  # declarate function
-    return f'<{tag}>{word}</{tag}>'  # declarate output pattern
+    return f"<{tag}>{word}</{tag}>"  # declarate output pattern
 
 
-print(html('i', 'Yay'))  # work with string and tag
-print(html('i', 'Hello'))  # work with string and tag
-print(html('cite', 'Yay'))  # work with string and tag
-print('______________________')  # separate tasks results
+print(html("i", "Yay"))  # work with string and tag
+print(html("i", "Hello"))  # work with string and tag
+print(html("cite", "Yay"))  # work with string and tag
+print("______________________")  # separate tasks results
 
 """
-Return the "centered" average of an array of ints, which we'll say is the mean average of the values, 
-except ignoring the largest and smallest values in the array. 
-If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value. 
+Return the "centered" average of an array of ints, which we'll say is the mean average of the values,
+except ignoring the largest and smallest values in the array.
+If there are multiple copies of the smallest value, ignore just one copy, and likewise for the largest value.
 Use int division to produce the final average. You may assume that the array is length 3 or more.
 [1, 2, 3, 4, 100] → 3
 [1, 1, 5, 5, 10, 8, 7] -> 5
@@ -146,10 +157,16 @@ def centered_average(nums):  # declaration the function
     return total // count  # calculating the average
 
 
-print(centered_average([1, 2, 3, 4, 100]))  # print reworked array according function logic
-print(centered_average([1, 1, 5, 5, 10, 8, 7]))  # print reworked array according function logic
-print(centered_average([-10, -4, -2, -4, -2, 0]))  # print reworked array according function logic
-print('______________________')  # separate tasks results
+print(
+    centered_average([1, 2, 3, 4, 100])
+)  # print reworked array according function logic
+print(
+    centered_average([1, 1, 5, 5, 10, 8, 7])
+)  # print reworked array according function logic
+print(
+    centered_average([-10, -4, -2, -4, -2, 0])
+)  # print reworked array according function logic
+print("______________________")  # separate tasks results
 
 """
 Remove all duplicates from a given string
@@ -171,7 +188,7 @@ def remove_duplicates(input_string):  # function declaration
 input_str = "Hello world!"  # string to work with
 output_str = remove_duplicates(input_str)  # output string according no duplicate logic
 print(output_str)  # print output string
-print('______________________')  # separate tasks results
+print("______________________")  # separate tasks results
 
 """
 Find a character with maximal frequency in given String and how many times it appears in this String
@@ -184,7 +201,9 @@ Output : [['t', 4], ['e', 4], [' ', 4]]
 def find_max_frequency(input_string):  # declarate function
     freq = Counter(input_string)  # create counter object to count each letter
     max_freq = max(freq.values())  # find the max number of counts
-    max_freq_chars = [[char, count] for char, count in freq.items() if count == max_freq]
+    max_freq_chars = [
+        [char, count] for char, count in freq.items() if count == max_freq
+    ]
     # creating the list and count of max counted chars
     return max_freq_chars  # return this list
 
