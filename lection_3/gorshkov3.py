440a62f7ac5e86b69ac85f441a3ada495ed38fcb
NBK-278\Aleksandr Gorshkov <aleksandr.gorshkov@chisw.com>
2024-06-07 17:18:53 +0300

    all current tasks are done

--- lection_3/gorshkov3.py
+++ lection_3/gorshkov3.py
@@ -182,7 +182,42 @@ If the input is valid, perform the calculation and print out the result.
 """
 
 
-print("task 6 calculator         ____________")
+print("task 6 calculator         ________DONE")
+
+
+def calculator():
+    while True:
+        print("""Введите первый операнд, математическое действие
+         и второй операнд (22 + 22) или 'exit' для выхода:""")
+        operation = input()
+        if operation == 'exit':
+            break
+        else:
+            print("ok")
+        formula = operation.split()
+        if (len(formula) == 3 and formula[0].isdigit()
+                and formula[2].isdigit() and
+                formula[1] in ['+', '-', '*', '/']):
+            num1 = int(formula[0])
+            num2 = int(formula[2])
+            operator = formula[1]
+            if operator == '+':
+                result = num1 + num2
+            elif operator == '-':
+                result = num1 - num2
+            elif operator == '*':
+                result = num1 * num2
+            elif operator == '/':
+                try:
+                    result = num1 / num2
+                except ZeroDivisionError:
+                    print('Ошибка: Деление на ноль')
+            print(f"Результат: {result}")
+        else:
+            print('Неправильная формула')
+
+
+calculator()
 
 
 """
